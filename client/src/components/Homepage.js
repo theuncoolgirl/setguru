c107f05d235278a034f4f8c25528db6da0586a0d
Erin Shields <elshields92@gmail.com>
2020-10-02 15:57:15 -0300

    Update homepage styling

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Button, InputBase, Grid, Typography } from '@material-ui/core';
+import { Button, Container, Grid, InputBase, Paper, Typography } from '@material-ui/core';
 import { fade, makeStyles } from '@material-ui/core/styles';
 import { actions } from '../store/search';
 import background from '../images/concert.jpg';
+import flatliners from '../images/flatliners.jpg';
+import againstMe from '../images/againstMe.png';
+import pup from '../images/pup.jpg';
+// import pupGif from '../images/video2.gif';
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
+    root: {
+        flexGrow: 1,
+        maxWidth: 1170
     },
     formItem: {
         backgroundColor: fade(theme.palette.common.white, 0.35),
@@ -27,6 +30,62 @@ const useStyles = makeStyles((theme) => ({
         justifyContent: "center",
         alignItems: "center",
         flexDirection: "column"
+    },
+    artist: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        height: 195,
+        width: 325,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        backgroundImage: `url(${flatliners})`,
+        backgroundSize: "130%",
+        backgroundPosition: "60% 50%",
+    },
+    flatliners: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        height: 195,
+        width: 325,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        backgroundImage: `url(${flatliners})`,
+        backgroundSize: "130%",
+        backgroundPosition: "60% 50%",
+    },
+    againstMe: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        height: 195,
+        width: 325,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        backgroundImage: `url(${againstMe})`,
+        backgroundSize: "100%",
+        backgroundPosition: "40% 5%",
+    },
+    pup: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        height: 195,
+        width: 325,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        backgroundImage: `url(${pup})`,
+        backgroundSize: "125%",
+        backgroundPosition: "60% 50%",
     },
     title: {
         color: 'white',
@@ -59,13 +118,30 @@ function Homepage(props) {
         <div>
             <div className={classes.hero}>
                 <Typography className={classes.title} variant="h6" noWrap>Find setlists for your favorite artists</Typography>
-                <form className={classes.root} noValidate autoComplete="off">
+                <form noValidate autoComplete="off">
                     <InputBase className={classes.search} id="searchBar" placeholder="  Artist, Venue, Location..." onChange={props.updateSearchValue} />
                     {/* ToDO: disable enter key */}
                     <Button className={classes.formItem} variant="contained" color="primary" onClick={() => window.location.href = `/search/${searchQuery}/1`}>Search</Button>
                 </form>
             </div>
-            <h1 className={classes.homepageTitle}>Homepage Component</h1>
+            <Container>
+                <div>
+                    <Typography className={classes.homepageTitle} variant="h6" noWrap>Artists to Know</Typography>
+                </div>
+                <Grid container justify="space-between" className={classes.root}>
+                    <Grid item xs={3}>
+                        <Paper className={classes.flatliners} />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Paper className={classes.againstMe} />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Paper className={classes.pup} />
+                    </Grid>
+                </Grid>
+                <h1 className={classes.homepageTitle}>Bottom</h1>
+            </Container>
+
 
         </div>
     );
